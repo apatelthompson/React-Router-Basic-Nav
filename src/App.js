daf1dada7cb9d287ca581a938d4cdcadcc01387e
avni <avni@hellorosey.com>
2019-07-21 22:12:21 +0200

    added React Router and added routes and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+import { Route } from "react-router-dom";
+<Route exact path="/Home" component={Home} />;
+<Route path="/Contact" component={Contact} />;
+<Route path="/About" component={About} />;
 
 const App = () => (
   <div>
     <Navigation />
   </div>
 );
-
 export default App;

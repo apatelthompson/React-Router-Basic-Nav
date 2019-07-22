d7797553ae4d7a22ad99951bafe37c152b63e882
avni <avni@hellorosey.com>
2019-07-22 08:54:39 +0200

    completed MVP

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
-<Route exact path="/Home" component={Home} />;
-<Route path="/Contact" component={Contact} />;
-<Route path="/About" component={About} />;
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={Home} />;
+    <Route path="/contact" component={Contact} />;
+    <Route path="/about" component={About} />;
   </div>
 );
+
 export default App;
